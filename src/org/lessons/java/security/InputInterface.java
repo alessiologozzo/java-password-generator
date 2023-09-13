3df7a15af6d72339383dc10ba72635a64d050e89
alessiologozzo <alessio.logozzo@libero.it>
2023-09-13 17:33:38 +0200

    Project completed

---
+++ src/org/lessons/java/security/InputInterface.java
@@ -0,0 +1,94 @@
+package org.lessons.java.security;
+
+import java.util.Scanner;
+
+public class InputInterface {
+
+	public static String getString(Scanner scanner, String requestMessage) {
+		String s = null, resultString = "";
+		boolean inputSuccess = false;
+
+		System.out.println(requestMessage);
+
+		while (!inputSuccess) {
+
+			s = scanner.nextLine();
+
+			s = s.trim();
+			// Remove white spaces inside the string and replace them with a '-', accepting
+			// only letters
+			for (int i = 0; i < s.length(); i++)
+				if ((s.charAt(i) >= (char) 65 && s.charAt(i) <= (char) 90)
+						|| (s.charAt(i) >= (char) 97 && s.charAt(i) <= (char) 122))
+					resultString += s.charAt(i);
+				else if (s.charAt(i) == ' ') {
+					if (i + 1 < s.length())
+						if (s.charAt(i) == ' ' && s.charAt(i + 1) != ' ')
+							resultString += '-';
+				} else {
+					resultString = "";
+					break;
+				}
+
+			if (resultString.isBlank())
+				System.out.println("Errore! Non sono ammessi numeri o caratteri speciali. Riprova");
+			else
+				inputSuccess = true;
+
+		}
+
+		return resultString;
+	}
+
+	public static int getInt(Scanner scanner, int min, int max, String requestMessage) {
+
+		String inputValue;
+		int x = 0;
+		boolean inputSuccess = false;
+
+		System.out.println(requestMessage);
+
+		while (!inputSuccess) {
+			inputValue = scanner.nextLine();
+			try {
+				x = Integer.parseInt(inputValue);
+
+				if (x < min)
+					System.out.println("Errore! Devi inserire un numero maggiore di " + (min - 1) + ". Riprova");
+				else if (x > max)
+					System.out.println("Errore! Devi inserire un numero minore di  " + (max + 1) + ". Riprova");
+				else
+					inputSuccess = true;
+			} catch (NumberFormatException e) {
+				System.out.println("Errore! Devi inserire un numero intero. Riprova");
+			}
+		}
+
+		return x;
+	}
+
+	public static int getCorrectDay(Scanner scanner, int month, String requestMessage) {
+		int max = 0;
+		int x = 0;
+
+		switch (month) {
+
+		case 2:
+			max = 28;
+			break;
+
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			max = 30;
+			break;
+
+		default:
+			max = 31;
+		}
+
+		x = getInt(scanner, 1, max, requestMessage);
+		return x;
+	}
+}
---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,25 @@
+package org.lessons.java.security;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String name = null, surname = null, favoriteColor = null, birthDate = null, password = null;
+		int day = 0, month = 0, year = 0;
+		
+		name = InputInterface.getString(scanner, "Inserisci il tuo nome");
+		surname = InputInterface.getString(scanner, "Ora inserisci il tuo cognome");
+		favoriteColor = InputInterface.getString(scanner, "Qual è il tuo colore preferito?");
+		year = InputInterface.getInt(scanner, 1900, 2023, "Che anno sei nato?");
+		month = InputInterface.getInt(scanner, 1, 12, "In che mese del " + year + " sei nato?");
+		day = InputInterface.getCorrectDay(scanner, month, "In che giorno sei nato?");
+		
+		password = name + "-" + surname + "-" + favoriteColor + "-" + (day + month + year);
+		System.out.println("Password generata con successo! La password generata per te è: " + password + System.lineSeparator() + "Arrivederci!");
+		
+		scanner.close();
+	}
+
+}
